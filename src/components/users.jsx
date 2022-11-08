c23e890b408c0f24fcd0d8e80917b8dff2d3c466
Sample18 <Sample417@yandex.ru>
2022-11-08 23:45:40 +0300

    create table with users and add styles

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -4,7 +4,55 @@ import API from '../api';
 const Users = () => {
     const [users, setUsers] = useState(API.users.fetchAll())
 
-    return
+    const numWord = (length) => {
+        const words = ['человек', 'человека', 'человек']
+        const value = Math.abs(length) % 100; 
+        const num = value % 10;
+        if(value > 10 && value < 20) return words[2]; 
+        if(num > 1 && num < 5) return words[1];
+        if(num === 1) return words[0]; 
+        return words[2];
+    }
+
+    const handlerDelete = (id) => {
+        setUsers(users.filter(user => user._id !== id))
+    }
+
+    const renderUsers = () => {
+        return (
+            users.length > 0 ? 
+            <>
+            <span className="badge bg-primary fs-4">{users.length} {numWord(users.length)} тусанет с тобой сегодня</span>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col">Имя</th>
+                    <th scope="col">Качества</th>
+                    <th scope="col">Профессия</th>
+                    <th scope="col">Встретился, раз</th>
+                    <th scope="col">Оценка</th>
+                    <th scope="col"></th>
+                </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => 
+                        <tr key={user._id}>
+                            <td>{user.name}</td>
+                            <td>{user.qualities.map(q => <span key={q._id} className={'m-1 badge bg-'+q.color}>{q.name}</span>)}</td>
+                            <td>{user.profession.name}</td>
+                            <td>{user.completedMeetings}</td>
+                            <td>{user.rate}/5</td>
+                            <td><button className='m-1 btn btn-danger' onClick={() => handlerDelete(user._id)}>delete</button></td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+            </>
+            : <span className="badge bg-danger fs-4">Никто с тобой не пойдет</span>
+        )
+    }
+    
+    return renderUsers()
 }
 
 export default Users
