6f4d5a2ab39f1f2fd3d92ae919d64f2cdb356fd3
Sample18 <Sample417@yandex.ru>
2022-11-23 22:33:33 +0300

    added proptypes

--- src/components/bookmark.jsx
+++ src/components/bookmark.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Bookmark = (props) => {
+const Bookmark = ({ bookmark, _id, onChangeBookmark }) => {
     return (
         <i
-            className={`bi bi-toggle-${props.bookmark ? "on" : "off"} fs-4 m-4`}
-            onClick={() => props.onChangeBookmark(props._id)}
+            className={`bi bi-toggle-${bookmark ? "on" : "off"} fs-4 m-4`}
+            onClick={() => onChangeBookmark(_id)}
         ></i>
     );
+};
+
+Bookmark.propTypes = {
+    bookmark: PropTypes.bool.isRequired,
+    _id: PropTypes.number.isRequired,
+    onChangeBookmark: PropTypes.func.isRequired
 };
 
 export default Bookmark;
